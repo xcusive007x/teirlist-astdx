c105af3cf9b3997def484e544088a3cede6477de
xcusive007x <143580398+xcusive007x@users.noreply.github.com>
2025-07-07 14:39:06 +0700

    Update server-simple.js

--- server-simple.js
+++ server-simple.js
@@ -439,8 +439,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
 module.exports = app;
