40050d7cfba6aa1cfef4fd558c563bf07c883e4d
xcusive007x <143580398+xcusive007x@users.noreply.github.com>
2025-07-07 13:59:16 +0700

    Update server-simple.js

--- server-simple.js
+++ server-simple.js
@@ -435,7 +435,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 // Start server
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
