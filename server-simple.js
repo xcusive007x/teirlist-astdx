8489c261cefe7e14152172f7cff3a730e4c0abd6
xcusive007x <143580398+xcusive007x@users.noreply.github.com>
2025-07-07 14:14:14 +0700

    Update server-simple.js

--- server-simple.js
+++ server-simple.js
@@ -3,7 +3,7 @@ const path = require("path");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
